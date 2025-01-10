aa95a2cccbc6df13bd504326eaf91c035c45b589
Felixnganga-max <felixngunga3620@gmail.com>
2025-01-11 00:37:30 +0300

    w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "SSTI",
+  title: "Serenity",
   description: "Satisfactory solutions for a better tomorrow",
 };
 
