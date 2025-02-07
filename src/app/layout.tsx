c546bfb4184e64a8319c42effb05f438d593c93c
Felixnganga-max <felixngunga3620@gmail.com>
2025-02-07 09:43:41 +0300

    Added Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,13 @@ const calistoga = Calistoga({
 });
 
 export const metadata: Metadata = {
-  title: "Serenity",
+  title: "Serenity Hi-Tech",
   description: "Satisfactory solutions for a better tomorrow",
+  icons: {
+    icon: "/logo.png", // Main icon
+    shortcut: "/logo.png", // For older browsers
+    apple: "/logo.png", // Apple devices
+  },
 };
 
 export default function RootLayout({
@@ -23,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/logo.png" sizes="32x32 48x48 64x64" />
+
+        <link rel="icon" href="/logo.png" type="image/svg+xml" />
+
+        <link rel="apple-touch-icon" href="/logo.png" sizes="180x180" />
+        <meta name="theme-color" content="#ffffff" />
+      </head>
       <body
         className={twMerge(
           inter.variable,
